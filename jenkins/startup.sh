12771ef3ee4341056a753bd6e572c89670aacc72
Walker-PI <1443045903@qq.com>
2020-10-10 00:51:46 +0800

    test
    Change-Id: I3322b1a5cb6c5592c37b783d27cc4070d31036b8

--- jenkins/startup.sh
+++ jenkins/startup.sh
@@ -4,8 +4,6 @@ export PROJ_PATH=`pwd`
 
 ssh admin@47.103.195.52
 
-cd $PROJ_PATH
-mvn docker:build
 docker push registry.cn-hangzhou.aliyuncs.com/walkerlms/eureka:0.0.1
 
 docker service create --replica 1 --name eureka -p 8080:8080 registry.cn-hangzhou.aliyuncs.com/walkerlms/eureka:0.0.1
